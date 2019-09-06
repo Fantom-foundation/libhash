5f93b51028c1f8dfd089c3a91ff27dbfd07d35af
Aryeh <aryehzinn@hotmail.com>
2019-09-06 13:36:52 +1000

    Added Vladimir's changes and edited markdown

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,4 @@
-/// # Fantom/libHash/errors
-///
-/// This file defines an error variant which can be used for the Hash trait.
-///
+//! This file defines an error variant which can be used for the [`Hash`](https://github.com/Fantom-foundation/libhash/blob/master/src/lib.rs) trait.
 #[derive(Debug)]
 pub enum Error {
     /// A serialization error in `compute_hash`.
