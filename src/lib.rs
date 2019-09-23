e793e0232295f0765692bfe1c2f536b2878a0d84
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-09-23 12:45:16 +1000

    add trait using in EventHash of libconsensus-dag

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use crate::errors::Error;
 use serde::Serialize;
+use to_vec::ToVec;
 
 pub trait Hashing {
-    type Hash: AsRef<[u8]>;
+    type Hash: AsRef<[u8]> + ToVec<u8> + Default;
     /// creates new Hash out of digest provided
     fn new(digest: &[u8]) -> Self::Hash;
     /// creates new Hash out of serialisable data provided
