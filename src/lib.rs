52170c900e929d3325b23dc0e4f6d84a754b1854
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-09-23 13:52:50 +1000

    add Hash and Copy traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 //! This crate defines a trait which contains commonly used behaviours for hashing.
 //! An example of an implementation of this trait can be found in the [`libhash-sha3`](https://github.com/Fantom-foundation/libhash-sha3) crate.
 use crate::errors::Error;
+use core::hash::Hash;
 use serde::Serialize;
 use to_vec::ToVec;
 
 /// The Hashing trait. So far, the trait requires an array of 8-bit unsigned integers as a defined
 /// type.
 pub trait Hashing {
-    type Hash: AsRef<[u8]> + ToVec<u8> + Default;
+    type Hash: AsRef<[u8]> + ToVec<u8> + Default + Hash + Copy;
     /// Creates new Hash out of the provided digest.
     fn new(digest: &[u8]) -> Self::Hash;
     /// Creates a new hash from the inputted data. Data must be serializable for hash to function.
