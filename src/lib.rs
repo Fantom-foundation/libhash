397ff3c7b36371a8ce316fe24d4deeae5c9056dd
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-09-24 14:50:39 +1000

    rename Hashing to Hash; add PartialEq trait as requirement for an implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 //! This crate defines a trait which contains commonly used behaviours for hashing.
 //! An example of an implementation of this trait can be found in the [`libhash-sha3`](https://github.com/Fantom-foundation/libhash-sha3) crate.
 use crate::errors::Error;
-use core::hash::Hash;
+use core::hash::Hash as CoreHash;
 use serde::Serialize;
 use to_vec::ToVec;
 
 /// The Hashing trait. So far, the trait requires an array of 8-bit unsigned integers as a defined
 /// type.
-pub trait Hashing {
-    type Hash: AsRef<[u8]> + ToVec<u8> + Default + Hash + Copy;
+pub trait Hash: AsRef<[u8]> + ToVec<u8> + Default + CoreHash + Copy + PartialEq {
     /// Creates new Hash out of the provided digest.
-    fn new(digest: &[u8]) -> Self::Hash;
+    fn new(digest: &[u8]) -> Self
+    where
+        Self: Sized;
     /// Creates a new hash from the inputted data. Data must be serializable for hash to function.
-    fn hash<D: Serialize>(data: &D) -> Result<Self::Hash, Error>;
+    fn hash<D: Serialize>(data: &D) -> Result<Self, Error>
+    where
+        Self: Sized;
 }
 
 pub mod errors;
